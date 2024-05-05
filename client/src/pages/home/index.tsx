a3abd8023821bf57c4fb84dab96f1379684980f8
harshbhane0 <harshbhange123@gmail.com>
2024-05-05 17:24:48 +0530

    "Added authentication check to home page, redirecting to auth page if user is not logged in"

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -1,3 +1,13 @@
+import { Auth } from "@/firebase/config";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function Home() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!Auth.currentUser?.email) {
+      navigate("/auth");
+    }
+  }, []);
   return <div>Home</div>;
 }
